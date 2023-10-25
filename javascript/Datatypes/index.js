da274a185a73c3d4f8de7cd303af413fb966d283
ashok <ashok5415.v@gmail.com>
2023-10-25 15:05:25 +1100

    datatypes

---
+++ javascript/Datatypes/index.js
@@ -0,0 +1,63 @@
+// string
+const name= "Ash";
+console.log(typeof name);
+
+// Number
+
+const Number= 45;
+console.log(typeof Number);
+
+// Boolean
+
+const haskids=true;
+console.log(typeof haskinds);
+
+// Null
+
+const car =null;
+
+//Symbol
+
+const sym = Symbol();
+
+console.log(typeof sym);
+
+// Reference Types
+
+const hobbies=['movies','music'];
+console.log(typeof hobbies);
+
+const details={
+    name: 'Ashok',
+    city: 'melbourne',
+    job: 'developer'
+};
+
+console.log(typeof details);
+console.log(details.city);
+
+// number to string
+
+let val;
+
+val=555;
+console.log(typeof val);
+
+val= String(555);
+
+console.log(typeof val);
+
+// Boolean to string
+
+val =String(true);
+console.log(typeof val);
+
+//date to string
+
+val=String(newDate());
+console.log(typeof val);
+
+// Array to string
+
+val=String([1,2,3,4]);
+console.log(typeof val);

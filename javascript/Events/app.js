c990e0382805d49b9bb5c2cdb8be1cbb938bde48
ashok <ashok5415.v@gmail.com>
2023-11-01 12:06:27 +1100

    events

---
+++ javascript/Events/app.js
@@ -0,0 +1,61 @@
+// document.querySelector('.clear').addEventListener('click',
+//     function(e){
+//     console.log('hello world');
+// });
+
+// if you want to add a name to the function 
+
+//   document.querySelector('.clear').addEventListener('click', onclick);
+
+//   function onclick(e){
+//     console.log(`${'hello World'}`);
+
+//     let val;
+//     val=e;
+    // // val=e.target;
+//     console.log(val);
+//   };
+
+  // event type
+
+//   val= e.type;
+
+  // time stamp
+
+//   val= e.timStamp;
+
+//   console.log(val);
+
+
+  // mouse events
+
+  //const clearBtn= document.querySelector('clear');
+
+  //const heading= document.querySelector('#id');
+
+  // click event
+
+  //clearBtn.addEventListener('click',runEvent);
+// double click event
+  //clearBtn.addEventListener('dblclick',runEvent);
+
+  // mousedown event 
+
+  //clearBtn.addEventListener('mousedown',runEvent);
+
+  // mouseup event
+  //clearBtn.addEventListener('mouseup',runEvent);
+
+// mousemove event
+
+//clearBtn.addEventListener('mousemove',runEvent);
+
+// event handler
+//   function runEvent(event){
+//      console.log(event);
+//   }
+
+// pending topics
+
+// event bubbling
+// event delegation
---
+++ javascript/localstorage/index.js
@@ -0,0 +1,17 @@
+// set local storage
+
+localStorage.setItem('name', jon);
+
+// set sessionStorage
+
+sessionStorage.setItem('name', 'Ash');
+
+//set remove item
+
+localStorage.removeItem('name');
+
+// get localStorage
+
+const name= localStorage.getItem('name');
+
+console.log(name);

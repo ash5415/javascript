8a40b17f6d5b56a95e3a78e010939beb1f809ae3
Ashok Velpula <ashokvelpula@Ashoks-MacBook-Pro.local>
2024-03-06 13:12:31 +1100

    hotelbooking

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ Room.prototype.info =function(){
 
 let room1 = new Room(2,50,1);
 let room2 = new Room(1,30.2);
+let room3 = new Room(2,50,3);
 
 room1.info();
 room2.info();
+//room3.info();
 
 // task-4 add the book method to the room prototype
 
@@ -55,3 +57,54 @@ Room.prototype.changePrice = function(newPrice){
 }
 room1.changePrice(60);
 room1.info();
+   
+
+// task-6 create a room1,room2,room3 and hotel
+
+var hotel ={
+name: 'Ash Hotels',
+star: 5,
+rooms : [room1,room2.room3]
+}
+
+// task -7 Add the info method to the hotel
+
+hotel.info=function(){
+    for(var i=0; i<this.rooms.length; i++){
+        this.rooms[i].info();
+    }
+}
+hotel.info();
+
+
+// task-8 add the book method
+
+hotel.book = function(guest){
+   for(var i =0 ; i<this.rooms.length; i ++){
+    var roomFound = false;
+    if(!this.rooms[i].booked && this.rooms[i].capacity >= guest.capacity){
+        this.rooms[i].book();
+        roomFound =true;
+        break;
+
+    }
+    if(!roomFound){
+        console.log(`sorry there is no rooms`);
+    }
+   }
+}
+hotel.book(2);
+hotel.info();
+
+//task -9  add the check out method to the hotel
+
+hotel.checkout =function(number){
+       for(var i =0; i < this.rooms.length; i++ ){
+        if(this.rooms[i].number === rNumber){
+                this.rooms[i].checkout();
+                break;
+        }
+       }
+}
+hotel.checkout(1);
+hotel.info();
